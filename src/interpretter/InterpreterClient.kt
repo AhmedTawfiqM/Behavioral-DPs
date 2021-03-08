64afe6fe87e564ef3dd6dd26668eb64c25705bb6
Ahmed Tawfiq <AhmedTawfiqM>
2021-03-08 03:26:10 +0200

    Interpreter Design Pattern

--- src/interpretter/InterpreterClient.kt
+++ src/interpretter/InterpreterClient.kt
@@ -14,6 +14,7 @@ class InterpreterClient(private val interpreterContext: InterpreterContext) {
             }
             else -> return str
         }
+
         return exp.interpret(interpreterContext)
     }
 }
